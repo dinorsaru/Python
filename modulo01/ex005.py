b12a6879067cef913f335bad53c737c0e24b0fdd
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-22 15:50:44 -0300

    exercicios

---
+++ modulo01/ex005.py
@@ -0,0 +1,4 @@
+n=int(input('digite um numero: '))
+a=n-1
+b=n+1
+print('analisando seu numero {}, seu antecessor é {} e seu sucessor é {}'.format(n,a,b))
---
+++ modulo01/ex006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+d = n*2
+t = n*3
+r = n **(1/2)
+print(' o dobro de {} é {}'.format(n,d))
+print('o triplo de {} é {} e a rais quadrada é {}'.format(n,t,r))
