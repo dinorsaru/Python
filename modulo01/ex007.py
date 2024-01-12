d3e43924cfe40bb2eada2946d4594412b2919ebb
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2024-01-12 17:59:01 -0300

    update exercicios

---
+++ modulo01/ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input('digite a nota do aluno'))
+n2 = float(input('digite a nota do aluno'))
+media = n1 + n2 / 2
+print(media)
---
+++ modulo01/ex010.py
@@ -0,0 +1,3 @@
+real = float(input('quanto dinheiro voce tem? '))
+dolar = real / 3.27
+print(' com R${:.2f} voce pode comprar Us${:.2f}'.format(real, dolar))
